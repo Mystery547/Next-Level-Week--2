a36a49fd55373c05e5640898b3355c83c06247a6
ricardo <ricardofs547@live.com>
2020-08-07 19:46:56 -0300

    finished platform

--- src/server.js
+++ src/server.js
@@ -1,84 +1,7 @@
-const proffys = [
-    {
-        name: "Diego Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-        whatsapp: "40028922", 
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões",  
-        subject: "Química", 
-        cost: "20", 
-        weekday: [0],
-        time_from: [720], 
-        time_to: [1220]
-    },
-    {
-        name: "Mayk Brito",
-        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4", 
-        whatsapp: "40028922", 
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões",  
-        subject: "Química", 
-        cost: "20", 
-        weekday: [0],
-        time_from: [720], 
-        time_to: [1220]
-    }
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Cinências",
-    "Educação Física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química"
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado"
-]
-
-function getSubject(subjectNumber) {
-    const arrayPosition = subjects[subjectNumber - 1]
-    return arrayPosition
-}
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    const filters = req.query
-    return res.render("study.html", {proffys, filters, subjects, weekdays})
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-    const isNotEmpty = Object.keys(data).length > 0
-
-    if (isNotEmpty) {
-        data.subject = getSubject(data.subject)
-
-        proffys.push(data)
-
-        return res.redirect("/study")
-        
-    }
-    
-
-    return res.render("give-classes.html", {subjects, weekdays})
-}
-
 const express = require('express')
 const server = express()
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses} = require('./pages')
 
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
@@ -86,8 +9,11 @@ nunjucks.configure('src/views', {
     noCache: true,
 })
 
-server.use(express.static("public"))
+server
+.use(express.urlencoded({ extended: true }))
+.use(express.static("public"))
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 .listen(5500)
---
+++ src/utils/format.js
@@ -0,0 +1,39 @@
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Cinências",
+    "Educação Física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química"
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+]
+
+function getSubject(subjectNumber) {
+    const arrayPosition = subjects[subjectNumber - 1]
+    return arrayPosition
+}
+
+function convertHoursToMinutes(time) {
+    const [hour, minutes] = time.split(":")
+    return Number((hour * 60) + minutes)
+}
+
+module.exports = {
+    subjects,
+    weekdays,
+    getSubject,
+    convertHoursToMinutes
+}
